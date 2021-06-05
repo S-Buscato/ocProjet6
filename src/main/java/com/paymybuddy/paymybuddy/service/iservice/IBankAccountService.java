350dfdc332776c82d48fb993290d02c58a8d910f
S-Buscato <sylvain.buscato@gmail.com>
2021-06-06 00:15:17 +0200

    banckAccount TDD

--- src/main/java/com/paymybuddy/paymybuddy/service/iservice/IBankAccountService.java
+++ src/main/java/com/paymybuddy/paymybuddy/service/iservice/IBankAccountService.java
@@ -2,8 +2,19 @@ package com.paymybuddy.paymybuddy.service.iservice;
 
 import com.paymybuddy.paymybuddy.dto.BankAccountDTO;
 import com.paymybuddy.paymybuddy.exception.BankAccountExistsException;
-import com.paymybuddy.paymybuddy.models.BankAccount;
+
+import java.util.List;
 
 public interface IBankAccountService {
     BankAccountDTO save(BankAccountDTO bankAccountDTO, Long usersId) throws BankAccountExistsException;
+
+    List<BankAccountDTO> findAll(Long id);
+
+    BankAccountDTO findById(long id);
+
+    BankAccountDTO findByIban(String iban);
+
+    BankAccountDTO update(Long id, BankAccountDTO bankAccountDTO);
+
+    Long deleteById(long id);
 }
